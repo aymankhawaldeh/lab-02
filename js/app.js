81612e11e60985c809b23104b79227d800946845
mohammad alsboul <mohammad.alspoole@gmail.com>
2019-12-16 23:10:34 +0200

    still working

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
-'use strict'
-
 $('document').ready(function(){
-    
-    
-    function Story(data){
-        this.image_url = data.image_url;
-        this.title = data.title;
-        this.description = data.description;
-        this.keyword = data.keyword;
-        this.horns = data.horns;
-    }
-    Story.prototype.render = function (){
-        let wholeStory = $('<div> <h2 class="hi"></h2> <img/> <p></p> </div>').clone();
+
+  function Story(data){
+    this.image_url = data.image_url;
+    this.title = data.title;
+    this.description = data.description;
+    this.keyword = data.keyword;
+    this.horns = data.horns;
+  }
+  Story.prototype.render = function (){
+    let wholeStory = $('<div> <h2 class="hi"></h2> <img/> <p></p> </div>').clone();
 
     wholeStory.find('img').attr('src', this.image_url);
     wholeStory.find('h2').text(this.title);
@@ -21,25 +18,22 @@ $('document').ready(function(){
 
 
 
-    
+
     $('#photo-template').append(wholeStory);
   }
-  
-  
-    
-$.get('data/page-1.json')
-.then( data => {
 
-    data.forEach(element => {
+
+
+  $.get('data/page-1.json')
+    .then( data => {
+
+      data.forEach(element => {
         let story = new Story(element);
         story.render();
         $('#choose').append('<option>' + element.keyword + '</option>' );
 
-     
-        
-       
+      });
     });
-});
 
 });
 
